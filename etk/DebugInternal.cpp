167829eb736b8ce276cd41cf921c6b912bcd5b44
Edouard DUPIN <yui.heero@gmail.com>
2012-11-10 14:32:14 +0100

    [NEW] extract the etk toolbox from ewol ==> became external

---
+++ etk/DebugInternal.cpp
@@ -0,0 +1,9 @@
+/**
+ * @author Edouard DUPIN
+ * 
+ * @copyright 2011, Edouard DUPIN, all right reserved
+ * 
+ * @license BSD v3 (see license file)
+ */
+
+const char * etkLibName = "Etk      ";
---
+++ etk/Stream.cpp
@@ -0,0 +1,15 @@
+/**
+ * @author Edouard DUPIN
+ * 
+ * @copyright 2011, Edouard DUPIN, all right reserved
+ * 
+ * @license BSD v3 (see license file)
+ */
+
+#include <etk/Stream.h>
+
+etk::CCout etk::cout;
+etk::CEndl etk::endl;
+etk::CHex etk::hex;
+etk::CStart etk::cstart;
+
