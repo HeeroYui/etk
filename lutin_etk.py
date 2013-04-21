91b9d605993c09836ba9acd26579fcd6b1029cbc
Edouard DUPIN <yui.heero@gmail.com>
2013-04-21 22:22:12 +0200

    [DEV] update 'lutin' build system

---
+++ lutin_etk.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+import lutinModule
+import lutinTools
+
+def Create(target):
+	# module name is 'edn' and type binary.
+	myModule = lutinModule.module(__file__, 'etk', 'LIBRARY')
+	# add the file to compile:
+	
+	
+	myModule.AddSrcFile([
+		'etk/Debug.cpp',
+		'etk/DebugInternal.cpp',
+		'etk/unicode.cpp',
+		'etk/unicodeTable.cpp',
+		'etk/Char.cpp',
+		'etk/UString.cpp',
+		'etk/Stream.cpp',
+		'etk/RegExp.cpp',
+		'etk/tool.cpp',
+		'etk/Noise.cpp'])
+	
+	
+	myModule.AddSrcFile([
+		'etk/math/Matrix4.cpp',
+		'etk/math/Vector2D.cpp',
+		'etk/math/Vector3D.cpp'])
+	
+	
+	myModule.AddSrcFile([
+		'etk/os/FSNode.cpp',
+		'etk/os/FSNodeRight.cpp',
+		'etk/os/Memory.cpp'])
+	
+	#ifeq ("$(TARGET_OS)","Windows")
+	#	myModule.AddSrcFile('etk/os/Mutex.Windows.cpp')
+	#	myModule.AddSrcFile('etk/os/Semaphore.Windows.cpp')
+	#else
+	myModule.AddSrcFile('etk/os/Mutex.Generic.cpp')
+	myModule.AddSrcFile('etk/os/Semaphore.Generic.cpp')
+	#endif
+	
+	
+	# name of the dependency
+	#ifeq ("$(PLATFORM)","Android")
+	myModule.AddModuleDepend(['linearmath', 'zip'])
+	
+	myModule.CompileFlags_CC([
+		'-Wno-write-strings',
+		'-Wall'])
+	# TODO : The other way is to remove this ...
+	# TODO : Fore release mode : the etk folder are absolutly not at the same position in the tree ...
+	#,'-DMODE_RELEASE'])
+	
+	
+	myModule.AddExportPath(lutinTools.GetCurrentPath(__file__))
+	
+	
+	# add the currrent module at the 
+	return myModule
+
+
