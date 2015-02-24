ac52bb523b752c235fb3309392561578eef98ea2
Edouard DUPIN <yui.heero@gmail.com>
2015-02-24 23:55:28 +0100

    [DEV] add placeholder

--- etk/boost_to_std/etk/functional.h
+++ etk/boost_to_std/etk/functional.h
@@ -6,5 +6,18 @@
 #include <boost/bind.hpp>
 #include <boost/bind/placeholders.hpp>
 
+namespace boost {
+	namespace placeholders {
+		boost::arg<1> _1;
+		boost::arg<2> _2;
+		boost::arg<3> _3;
+		boost::arg<4> _4;
+		boost::arg<5> _5;
+		boost::arg<6> _6;
+		boost::arg<7> _7;
+		boost::arg<8> _8;
+		boost::arg<9> _9;
+	}
+}
 
 #endif
