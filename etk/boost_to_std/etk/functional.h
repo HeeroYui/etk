ea899a1ed1916d5fe815e1c1b8b54eee379b7d55
Edouard DUPIN <yui.heero@gmail.com>
2015-02-25 21:28:07 +0100

    [DEBUG] correct include of boost placeholder

--- etk/boost_to_std/etk/functional.h
+++ etk/boost_to_std/etk/functional.h
@@ -8,15 +8,15 @@
 
 namespace boost {
 	namespace placeholders {
-		boost::arg<1> _1;
-		boost::arg<2> _2;
-		boost::arg<3> _3;
-		boost::arg<4> _4;
-		boost::arg<5> _5;
-		boost::arg<6> _6;
-		boost::arg<7> _7;
-		boost::arg<8> _8;
-		boost::arg<9> _9;
+		static boost::arg<1> _1;
+		static boost::arg<2> _2;
+		static boost::arg<3> _3;
+		static boost::arg<4> _4;
+		static boost::arg<5> _5;
+		static boost::arg<6> _6;
+		static boost::arg<7> _7;
+		static boost::arg<8> _8;
+		static boost::arg<9> _9;
 	}
 }
 
