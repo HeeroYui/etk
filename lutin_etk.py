9b5c977be175f243171c8c5ca7b3f5c7728b6191
Edouard DUPIN <yui.heero@gmail.com>
2015-05-08 22:40:35 +0200

    [BUILD] update new lutin 0.5.3

--- lutin_etk.py
+++ lutin_etk.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-import lutinModule as module
-import lutinTools as tools
+import lutin.module as module
+import lutin.tools as tools
 
 def get_desc():
 	return "e-tk : Ewol tool kit"
@@ -37,26 +37,26 @@ def create(target):
 	if target.config["mode"] == "release":
 		# TODO : The other way is to remove this ...
 		# TODO : Fore release mode : the etk folder are absolutly not at the same position in the tree ...
-		myModule.compile_flags_CC("-DMODE_RELEASE")
+		myModule.compile_flags('c', "-DMODE_RELEASE")
 	else:
-		myModule.add_export_flag_CC("-DDEBUG_LEVEL=3")
-		myModule.add_export_flag_CC("-DDEBUG=1")
+		myModule.add_export_flag('c', "-DDEBUG_LEVEL=3")
+		myModule.add_export_flag('c', "-DDEBUG=1")
 		# Bor backtrace display :
 		if target.name != "Windows":
-			myModule.add_export_flag_LD("-ldl -rdynamic")
+			myModule.add_export_flag('link', "-ldl -rdynamic")
 	
 	# for ald C++ compatibility (old GCC) just link with boost ...
 	if     target.config["compilator"] == "gcc" \
 	   and target.xx_version < 4007000:
 		# note : this framework depend on C++ 11, but a simple port of Boost for old compatibility has been done ...
 		myModule.compile_version_XX(1999)
-		myModule.add_optionnal_module_depend('boost', "ETK_BUILD_BOOST", export=True)
+		myModule.add_optionnal_module_depend('boost', ["c++", "-DETK_BUILD_BOOST"], export=True)
 		myModule.add_export_path(tools.get_current_path(__file__) + "/binding_boost")
 	else:
 		myModule.compile_version_XX(2011)
 		# name of the dependency
-		myModule.add_optionnal_module_depend('minizip', "ETK_BUILD_MINIZIP")
-		myModule.add_optionnal_module_depend('linearmath', "ETK_BUILD_LINEARMATH", export=True)
+		myModule.add_optionnal_module_depend('minizip', ["c++", "-DETK_BUILD_MINIZIP"])
+		myModule.add_optionnal_module_depend('linearmath', ["c", "-DETK_BUILD_LINEARMATH"], export=True)
 		myModule.add_export_path(tools.get_current_path(__file__) + "/binding_X11")
 	
 	if target.name=="Windows":
@@ -64,7 +64,7 @@ def create(target):
 	elif target.name=="Android":
 		pass
 	else:
-		myModule.add_export_flag_LD("-lpthread")
+		myModule.add_export_flag('link', "-lpthread")
 	
 	myModule.add_export_path(tools.get_current_path(__file__))
 	
