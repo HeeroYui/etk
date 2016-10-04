f0fd073a24985a389a11f4484fed0ae3836dce41
Edouard DUPIN <yui.heero@gmail.com>
2016-10-04 23:41:29 +0200

    [DEV] Update new lutin 2.2.0 (no legacy support)

--- lutin_etk-test.py
+++ lutin_etk-test.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
-import lutin.module as module
+import lutin.debug as debug
 import lutin.tools as tools
-import datetime
 
 
 def get_type():
@@ -25,11 +24,14 @@ def get_compagny_name():
 def get_maintainer():
 	return "authors.txt"
 
-def create(target, module_name):
-	my_module = module.Module(__file__, module_name, get_type())
+def configure(target, my_module):
 	my_module.add_src_file([
-		'test/main.cpp'
-		])
-	my_module.add_depend(['etk', 'gtest', 'test-debug'])
-	return my_module
+	    'test/main.cpp'
+	    ])
+	my_module.add_depend([
+	    'etk',
+	    'gtest',
+	    'test-debug'
+	    ])
+	return True
 
--- lutin_etk.py
+++ lutin_etk.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import lutin.module as module
+import lutin.debug as debug
 import lutin.tools as tools
 
 
@@ -24,54 +24,54 @@ def get_maintainer():
 def get_version():
 	return "version.txt"
 
-def create(target, module_name):
-	my_module = module.Module(__file__, module_name, get_type())
+def configure(target, my_module):
 	my_module.add_extra_flags()
 	# add the file to compile:
 	my_module.add_src_file([
-		'etk/debug.cpp',
-		'etk/etk.cpp',
-		'etk/stdTools.cpp',
-		'etk/tool.cpp',
-		'etk/Noise.cpp',
-		'etk/Color.cpp',
-		'etk/math/Matrix2.cpp',
-		'etk/math/Matrix4.cpp',
-		'etk/math/Vector2D.cpp',
-		'etk/math/Vector3D.cpp',
-		'etk/math/Vector4D.cpp',
-		'etk/os/FSNode.cpp',
-		'etk/os/FSNodeRight.cpp',
-		'etk/archive/Archive.cpp',
-		'etk/archive/Zip.cpp'])
+	    'etk/debug.cpp',
+	    'etk/etk.cpp',
+	    'etk/stdTools.cpp',
+	    'etk/tool.cpp',
+	    'etk/Noise.cpp',
+	    'etk/Color.cpp',
+	    'etk/math/Matrix2.cpp',
+	    'etk/math/Matrix4.cpp',
+	    'etk/math/Vector2D.cpp',
+	    'etk/math/Vector3D.cpp',
+	    'etk/math/Vector4D.cpp',
+	    'etk/os/FSNode.cpp',
+	    'etk/os/FSNodeRight.cpp',
+	    'etk/archive/Archive.cpp',
+	    'etk/archive/Zip.cpp'])
 	
 	my_module.add_header_file([
-		'etk/etk.hpp',
-		'etk/types.hpp',
-		'etk/stdTools.hpp',
-		'etk/tool.hpp',
-		'etk/Noise.hpp',
-		'etk/Color.hpp',
-		'etk/Hash.hpp',
-		'etk/math/Matrix2.hpp',
-		'etk/math/Matrix4.hpp',
-		'etk/math/Vector2D.hpp',
-		'etk/math/Vector3D.hpp',
-		'etk/math/Vector4D.hpp',
-		'etk/os/Fifo.hpp',
-		'etk/os/FSNode.hpp',
-		'etk/os/FSNodeRight.hpp',
-		'etk/archive/Archive.hpp',
-		'etk/archive/Zip.hpp'])
+	    'etk/etk.hpp',
+	    'etk/types.hpp',
+	    'etk/stdTools.hpp',
+	    'etk/tool.hpp',
+	    'etk/Noise.hpp',
+	    'etk/Color.hpp',
+	    'etk/Hash.hpp',
+	    'etk/math/Matrix2.hpp',
+	    'etk/math/Matrix4.hpp',
+	    'etk/math/Vector2D.hpp',
+	    'etk/math/Vector3D.hpp',
+	    'etk/math/Vector4D.hpp',
+	    'etk/os/Fifo.hpp',
+	    'etk/os/FSNode.hpp',
+	    'etk/os/FSNodeRight.hpp',
+	    'etk/archive/Archive.hpp',
+	    'etk/archive/Zip.hpp'])
 	
 	# build in C++ mode
 	my_module.compile_version("c++", 2011)
 	# add dependency of the generic C++ library:
-	my_module.add_depend('cxx')
-	# add dependency of the generic math library:
-	my_module.add_depend('m')
-	my_module.add_depend('elog')
-	my_module.add_depend('ememory')
+	my_module.add_depend([
+	    'cxx',
+	    'm',
+	    'elog',
+	    'ememory'
+	    ])
 	# add some optionnal libraries
 	my_module.add_optionnal_depend('minizip', ["c++", "-DETK_BUILD_MINIZIP"])
 	my_module.add_optionnal_depend('linearmath', ["c", "-DETK_BUILD_LINEARMATH"], export=True)
@@ -79,7 +79,7 @@ def create(target, module_name):
 	if "Android" in target.get_type():
 		my_module.add_depend("SDK")
 	
-	my_module.add_path(tools.get_current_path(__file__))
-	return my_module
+	my_module.add_path(".")
+	return True
 
 
