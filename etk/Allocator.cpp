467341b7df47d6bedba2f6c5ff098c5f6eae307f
Edouard DUPIN <yui.heero@gmail.com>
2017-10-14 11:12:46 +0200

    [DEV] add placement new if not define

--- etk/Allocator.cpp
+++ etk/Allocator.cpp
@@ -24,3 +24,16 @@ void operator delete[] (void* ptr) {
 	free(ptr);
 }
 */
+
+
+// it is define bu generic "include <new>" ==> no double define of placement new
+#ifndef _NEW
+	// Default placement versions of operator new (use char* instead of void* to permit no multiple definition).
+	inline void* operator new(size_t, char* _p) throw() {
+		ETK_VECTOR_DEBUG("plop\n");
+		return _p;
+	}
+	inline void operator delete (void*, char*) throw() {
+		
+	}
+#endif
